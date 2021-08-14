a17bcfa4bd54642048d1d7f3be83dd391c1438be
Jeff Smith <toxicglados@gmail.com>
2021-08-14 14:50:13 -0500

    Add option to combine all spaces into one

--- spaceify.py
+++ spaceify.py
@@ -7,17 +7,21 @@ import re
 
 if __name__ == "__main__":
     replacement = "----$$$$___"
-    target_file = sys.argv[1]
+    operation = sys.argv[1]
+    target_file = sys.argv[2]
+    assert operation == "spaceify" or operation == "despaceify"
     num_spaces = 2
     with open(target_file, 'r+') as target_file:
         file_content = target_file.read()
         # Get everything down to a single space
         file_content = re.sub(r"[ \t]+", " ", file_content)
-        # TODO: Check to make sure the replacement string doesn't already exist in the file somehow
-        # Put the replacement characters in there
-        file_content = file_content.replace(" ", replacement)
-        while replacement in file_content:
-            file_content = file_content.replace(replacement, " " * num_spaces, 1)
-            num_spaces += 1
+        if operation == "spaceify":
+            # TODO: Check to make sure the replacement string doesn't already exist in the file somehow
+            # Put the replacement characters in there
+            file_content = file_content.replace(" ", replacement)
+            while replacement in file_content:
+                file_content = file_content.replace(replacement, " " * num_spaces, 1)
+                num_spaces += 1
         target_file.seek(0)
+        target_file.truncate(0)
         target_file.write(file_content)
